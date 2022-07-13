c324de92dfb7bcf5c0fcfb0a442c472cece550a4
Tordi1 <josephgbarabe33@gmail.com>
2022-07-13 14:50:22 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copiar el contenido de uno a otra variable
+ * @dest: This is destiny
+ * @src: This is the copia
+ *
+ * Return: This is return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+	}
+	*dest = '\0';
+	return (start);
+}
+
