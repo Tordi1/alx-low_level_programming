eb6583af71eca4087f7ebff2b9c1c0f5e64389b8
Tordi1 <josephgbarabe33@gmail.com>
2022-07-13 08:23:13 -0700

    holberton.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n):
 	void swap_int(int *a, int *b);
 	int _strlen(char *s);
@@ -10,3 +13,4 @@ void reset_to_98(int *n):
 	char *_strcpy(char *dest, char *src);
 	void rev_string(char *s);
 
+#endif
